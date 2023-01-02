fc8363e573c1e58f1e5b05b1c1f6a7eeeaa5cb44
Karrar <whiteshark2012@gmail.com>
2023-01-02 21:13:31 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: Karrar <kahamza@student.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 20:50:13 by Karrar            #+#    #+#             */
-/*   Updated: 2023/01/02 21:09:12 by Karrar           ###   ########.fr       */
+/*   Updated: 2023/01/02 21:13:09 by Karrar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 #include <string.h>
 #include "libft.h"
 
-void    *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-    char    *buffer;
-    int        area;
+	char	*buffer;
+	int		area;
 
-    area = nmemb * size;
-    buffer = malloc(area);
-    if (!buffer)
-        return (NULL);
-    while (--area >= 0)
-        *(buffer + area) = '\0';
-    return ((void *)buffer);
+	area = nmemb * size;
+	buffer = malloc(area);
+	if (!buffer)
+		return (NULL);
+	while (--area >= 0)
+		*(buffer + area) = '\0';
+	return ((void *)buffer);
 }
